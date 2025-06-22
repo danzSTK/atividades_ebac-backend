1842d0512af3c645fb527d30e4d297934e686e30
Daniel_Félix <carlosdanielfelixfarias@gmail.com>
2025-06-22 11:53:30 -0300

    adicionando atividade do modulo 32

---
+++ atividade-modulo-32/src/test/java/com/mod32/Mod32ApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.mod32;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mod32ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
